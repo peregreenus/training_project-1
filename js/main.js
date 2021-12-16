9811ce471535d663e48dd2ae29d3c93f09b26d5d
peregreenus <coldlycalculatedsoul@gmail.com>
2021-12-17 00:03:20 +0300

    add: section #3 - Form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-$(function() {
+$(function () {
     $('.slider__inner').slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false,
     });
+
+    $('select').styler();
 });
